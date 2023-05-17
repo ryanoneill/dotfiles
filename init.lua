b37171954ef6b9f34d9c32f46e02c65f03c7fe42
Ryan O'Neill <ryanoneill@gmail.com>
2023-05-17 08:39:36 -0700

    Added Yank to Clipboard and Add Checkmark Shortcuts

--- init.lua
+++ init.lua
@@ -77,3 +77,9 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- Yank to Clipboard
+vim.keymap.set('n', '<leader>yc', '"+y')
+
+-- Paste Checkmark (with Space) at End of Row 
+vim.keymap.set('n', '<leader>ch', 'A ✓<esc>')

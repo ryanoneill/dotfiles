e82a70666ca69b2561f5f9f181c8204598343d76
Ryan O'Neill <ryanoneill@gmail.com>
2023-06-15 09:17:23 -0700

    Added Automatic Setup of Mason Installed LSP Servers

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -54,14 +54,33 @@ function M.capabilities()
   return cmp_nvim_lsp.default_capabilities()
 end
 
+function M.setup_server(lspconfig, capabilities, server)
+  local opts = {
+    capabilities = capabilities
+  }
+  lspconfig[server].setup(opts)
+end
+
+function M.setup_servers(mason_lspconfig)
+  local servers = mason_lspconfig.get_installed_servers()
+  local lspconfig = require("lspconfig")
+  local capabilities = M.capabilities()
+
+  for _, server in ipairs(servers) do
+    if server == "lua_ls" then
+      M.setup_lua(lspconfig, capabilities)
+    elseif server == "rust_analyzer" then
+      M.setup_rust(lspconfig, capabilities)
+    else
+      M.setup_server(lspconfig, capabilities, server)
+    end
+  end
+end
+
 function M.setup()
   local mason_lspconfig = require("mason-lspconfig")
   mason_lspconfig.setup()
-
-  local lspconfig = require("lspconfig")
-  local capabilities = M.capabilities()
-  M.setup_lua(lspconfig, capabilities)
-  M.setup_rust(lspconfig, capabilities)
+  M.setup_servers(mason_lspconfig)
 end
 
 return M

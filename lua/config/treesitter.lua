70e4389dc5671917234a7822a89bad429a3fd99c
Ryan O'Neill <ryanoneill@gmail.com>
2023-06-15 10:55:19 -0700

    Added Code Folding Based on Treesitter

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,4 +1,33 @@
+local vim = vim
+local api = vim.api
+local opt = vim.opt
+
 local M = {}
+
+function M.nvim_create_augroups(definitions)
+  for group_name, definition in pairs(definitions) do
+    api.nvim_command('augroup ' .. group_name)
+    api.nvim_command('autocmd!')
+    for _, def in ipairs(definition) do
+      local command = table.concat(vim.tbl_flatten({'autocmd', def}), ' ')
+      api.nvim_command(command)
+    end
+    api.nvim_command('augroup END')
+  end
+end
+
+function M.setup_code_folding()
+  opt.foldmethod = "expr"
+  opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+  local autoCommands = {
+    open_folds = {
+      {"BufReadPost,FileReadPost", "*", "normal zR"}
+    }
+  }
+
+  M.nvim_create_augroups(autoCommands)
+end
 
 function M.setup()
   local configs = require("nvim-treesitter.configs")
@@ -12,6 +41,7 @@ function M.setup()
     }
   }
   configs.setup(opts)
+  M.setup_code_folding()
 end
 
 return M
